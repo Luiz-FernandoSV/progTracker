4057f2fa30ecd6eea93bdd3d6b9c1d6e178664d2
Luiz Fernando <luiz.fernandosv23@gmail.com>
2025-08-05 15:24:09 -0300

    Protótipo da função de editar objetivos

--- script/functions/addObj.js
+++ script/functions/addObj.js
@@ -4,7 +4,13 @@ function adicionarObjeto(objetivo, conclusao) {
     // Adiciona um novo cartão ao container
     containerObjetivos.innerHTML += `
         <div class="cartao-objetivo" data-index="${objetivo.id}">
-                <h4 class="titulo-objetivo">${objetivo.titulo}</h4>
+                <div class="top-bar">
+                    <h4 class="titulo-objetivo">${objetivo.titulo}</h4>
+                    <div class="div-acoes">
+                        <i class="fa-solid fa-pen-to-square"></i>
+                        <i class="fa-solid fa-trash"></i>
+                    </div>
+                </div>
                 <p class="descricao-objetivo">${objetivo.descricao}</p>
                 <p class="sub-objetivos">Sub-Objetivos Cadastrados: ${objetivo.subobjetivos?.length ? objetivo.subobjetivos.length : '0'}</p>
                 <p class="conclusao-objetivo">Progresso: </p>
@@ -28,7 +34,27 @@ function adicionarObjeto(objetivo, conclusao) {
             // Leva o ID do objetivo como parâmetro na URL
             window.location.href = `./detalhes.html?id=${card.getAttribute('data-index')}`
         })
+        let iconeEditar = card.querySelector('.fa-pen-to-square');
+        iconeEditar.addEventListener('click', function (event) {
+            event.stopPropagation(); // Evita o redirecionamento
+            // Seleciona o modal e o form
+            let modal = document.querySelector('.container-modal');
+            let form = document.querySelector('form');
+            // Obtém os valores anteriores
+            let tituloAtual = card.querySelector('.titulo-objetivo').textContent;
+            let descricaoAtual = card.querySelector('.descricao-objetivo').textContent;
+            // Coloca os valores anteriores
+            modal.querySelector('input[id="titulo-obj"]').value = tituloAtual;
+            modal.querySelector('textarea[id="descricao-obj"]').value = descricaoAtual;
+
+            // Altera o modo para edição e adiciona o índice a ser alterado no data-index
+            form.dataset.mode = 'editar';
+            form.dataset.index = card.dataset['index'];
+            // Exibe o form modal
+            modal.style.display = 'flex';
+        })
     })
 }
+
 // Exporta a função
 export default adicionarObjeto;
--- script/home.js
+++ script/home.js
@@ -14,7 +14,7 @@ window.addEventListener('load', function () {
     data.objetivos.forEach(objetivo => {
         // Contador de subobjetivos concluídos
         let contadorConcluido = 0;
-        
+
         // Verifica se há subobjetivos
         if (objetivo.subobjetivos == null) {
             // Caso não haja marca o progresso como zerado
@@ -58,6 +58,9 @@ function fecharModal() {
     document.querySelector('.container-modal').style.display = 'none';
 }
 
+// Seleciona o modal
+const modal = document.querySelector('.container-modal');
+
 // Seleciona o botão de enviar
 const btn_enviar = document.querySelector('#btn-enviar');
 // Adiciona um eventListener
@@ -66,44 +69,82 @@ btn_enviar.addEventListener('click', function (event) {
     fecharModal()
     // Previne o recarregamento da página
     event.preventDefault()
-    // Obtém os campos do form
-    let objTitulo = document.querySelector('input[id="titulo-obj"]');
-    let objDescricao = document.querySelector('textarea[id="descricao-obj"]');
 
-    // Verifica se os campos estão vazios; caso estejam, retorna ao form
-    if (objTitulo.value == '' || objDescricao.value == '') {
-        alert("Por favor, preencha todos os campos corretamente.");
-        return;
+    // Variavel de controle
+    var modoForm = document.querySelector('form').dataset.mode;
+
+    if (modoForm == 'adicionar') {
+        // Obtém os campos do form
+        let objTitulo = document.querySelector('input[id="titulo-obj"]');
+        let objDescricao = document.querySelector('textarea[id="descricao-obj"]');
+
+        // Verifica se os campos estão vazios; caso estejam, retorna ao form
+        if (objTitulo.value == '' || objDescricao.value == '') {
+            alert("Por favor, preencha todos os campos corretamente.");
+            return;
+        }
+
+        // ID do novo objetivo, obtém o tamanho do array
+        let novoID = data.objetivos.length
+
+        // Cria um novo objeto
+        let novoObjetivo = {
+            id: novoID,
+            titulo: objTitulo.value,
+            descricao: objDescricao.value,
+            subobjetivos: [],
+            status: 'nao-concluido'
+        }
+        // Limpa os campos
+        objTitulo.value = '';
+        objDescricao.value = '';
+
+        // Busca os dados atuais do storage
+        let arrayAtual = JSON.parse(localStorage.getItem('dados')) || [];
+        // Atualiza o array
+        arrayAtual.objetivos.push(novoObjetivo);
+        // Atualiza o localstorage
+        localStorage.setItem('dados', JSON.stringify(arrayAtual))
+
+        // Chama a função para adicionar na tela
+        adicionarObjeto(novoObjetivo, 0);
+    } else if (modoForm == 'editar') {
+        // Caso o form esteja em modo de edição
+        // Seleciona o form e obtém o índice do card a ser alterado
+        let form = document.querySelector('form');
+        let cardIndex = form.dataset.index
+        // Seleciona o card com aquele índice
+        let card = document.querySelector(`.cartao-objetivo[data-index="${cardIndex}"]`);
+        // Obtém os novos valores
+        let novoTitulo = modal.querySelector('input[id="titulo-obj"]').value;
+        let novaDescricao = modal.querySelector('textarea[id="descricao-obj"]').value;
+
+        // Altera o HTML
+        card.querySelector('.titulo-objetivo').textContent = novoTitulo;
+        card.querySelector('.descricao-objetivo').textContent = novaDescricao;
+
+        // Obtém o array atual
+        let arrayAtual = JSON.parse(localStorage.getItem('dados')) || [];
+        // Obtém o objeto em questão
+        let objetivoAtual = arrayAtual.objetivos[cardIndex]
+        // Atualiza os valores
+        objetivoAtual.titulo = novoTitulo
+        objetivoAtual.descricao = novaDescricao
+
+
+        // Atualiza o localstorage
+        localStorage.setItem('dados', JSON.stringify(arrayAtual))
+
+        // Limpa os campos
+        modal.querySelector('input[id="titulo-obj"]').value = '';
+        modal.querySelector('textarea[id="descricao-obj"]').value = '';
+        // Reseta o modo do form e o índice
+        form.dataset.mode = 'adicionar';
+        form.dataset.index = ''
+    } else {
+        alert("Erro!")
     }
-
-    // ID do novo objetivo, obtém o tamanho do array
-    let novoID = data.objetivos.length
-
-    // Cria um novo objeto
-    let novoObjetivo = {
-        id: novoID,
-        titulo: objTitulo.value,
-        descricao: objDescricao.value,
-        subobjetivos: [],
-        status: 'nao-concluido'
-    }
-    // Limpa os campos
-    objTitulo.value = '';
-    objDescricao.value = '';
-
-    // Busca os dados atuais do storage
-    let arrayAtual = JSON.parse(localStorage.getItem('dados')) || [];
-    // Atualiza o array
-    arrayAtual.objetivos.push(novoObjetivo);
-    // Atualiza o localstorage
-    localStorage.setItem('dados', JSON.stringify(arrayAtual))
-
-    // Chama a função para adicionar na tela
-    adicionarObjeto(novoObjetivo,0);
 })
-
-// Seleciona o modal
-const modal = document.querySelector('.container-modal');
 
 // Adiciona um eventListener; se o usuário clicar fora do modal, ele será fechado
 window.addEventListener('click', function (event) {
